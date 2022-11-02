39c1f8c03c6cfd6f19a45f9c0beab4a58e63a840
Lordfedot <orestmazgal@gmail.com>
2022-11-02 18:23:21 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,72 +1,111 @@
-import './css/styles.css';
-import debounce from 'lodash.debounce';
-import fetchCountries from './fetchCountries'
+import Notiflix from 'notiflix';
+import {PhotoApiService} from './fetch'
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const DEBOUNCE_DELAY = 300;
+
+
 const refs = {
-    input: document.querySelector('#search-box'),
-    text: document.querySelector('.country-list'),
-    info: document.querySelector('.country-info')
+    form: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more')
 }
+const photoApiService = new PhotoApiService()
 
-refs.input.addEventListener('input', debounce(onInputChange,DEBOUNCE_DELAY))
+refs.form.addEventListener('submit', onFormSubmit)
+refs.loadMore.addEventListener('click', onLoadMoreClick)
+window.addEventListener('scroll' , async ()=> {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+        onLoadMoreClick()
+    }
+})
 
 
-function onInputChange(e) {
-    const name = e.target.value.toLowerCase().trim()
-    if (name === '') {
+
+async function onFormSubmit(e) {
+    e.preventDefault()
+    refs.loadMore.classList.add('is-hidden')
+
+    destroyMarkup()
+    photoApiService.query = e.target.elements.searchQuery.value.trim()
+    if (photoApiService.searchQuery === '') {
+        refs.loadMore.classList.add('is-hidden')
         destroyMarkup()
         return
     }
-    fetchCountries(name).then(countries => {
-        buildMarkUp(countries) 
-    }).catch(() =>{
-        destroyMarkup()
-    })
-}
+    photoApiService.resetPage()
+    photoApiService.fetchPhoto()
 
-function buildMarkUp(countries) {
-    const bigMarkup = countries.map(country => {
-        return createBigMarkUp(country)
-    }).join('')
-    const lineMarkup = countries.map(country => {
-        return createLineMarkUp(country)
-    }).join('')
-    if (countries.length < 2) {
-        refs.info.innerHTML = bigMarkup
-        refs.text.innerHTML = ''   
-    }else{
-        refs.text.innerHTML = lineMarkup
-        refs.info.innerHTML = ''
+    const data = await photoApiService.fetchPhoto()
+    const array = data.hits
+    if (array.length === 0) {
+        refs.loadMore.classList.add('is-hidden')
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        return
+    }
+    buildPhoto(array);
+    
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    
+    console.log(data);
+    if (array.length >= 40) {
+        refs.loadMore.classList.remove('is-hidden')
     }
 }
 
-function destroyMarkup(){
-    refs.text.innerHTML = ''
-    refs.info.innerHTML = ''
+async function onLoadMoreClick() {
+    const data = await photoApiService.fetchPhoto()
+    const array = data.hits
+    appendPhoto(array);
+
+    if (array.length < 40) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+        refs.loadMore.classList.add('is-hidden')
+    }
+    let infiniteScroll = new InfiniteScroll( '.container',{
+        path: photoApiService.fetchPhoto(),
+        append: appendPhoto()
+    })
+    console.log(data);
 }
 
-function createLineMarkUp({flags, name}) {
-    return `<li class="country-item">
-    <img class="country-img" width="30" height="30" src='${flags.png}' alt="${name.official} Flag">
-    <p>${name.official}</p>
-  </li>`
+function buildPhoto(array) {
+    const markup = array.map(photo => createMarkUp(photo)).join('')
+    refs.gallery.innerHTML = markup
+    const gallery = new SimpleLightbox('.gallery a');
 }
 
-function createBigMarkUp({flags, name, capital,population, languages}) {
-    const language = Object.values(languages)
-    return `<li class="country-item__coloumn">
-    <div class="country-name">
-        <img width="80" height="50" src='${flags.png}' alt="${name.official} Flag">
-        <p>${name.official}</p>
+function appendPhoto(array) {
+    const markup = array.map(photo => createMarkUp(photo)).join('');
+    refs.gallery.insertAdjacentHTML('beforeend',markup)
+    const gallery = new SimpleLightbox('.gallery a'); 
+    gallery.refresh() 
+}
+
+function destroyMarkup() {
+    refs.gallery.innerHTML = ''
+}
+
+function createMarkUp({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) {
+    return `<a class="gallery__image" href="${largeImageURL}">
+    <div class="photo-card">
+    <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>${downloads}
+      </p>
     </div>
-    <ul class="country-meta">
-      <li><p><span class="country-meta__title">Capital</span>:${capital}</p></li>
-      <li><p><span class="country-meta__title">Population</span>:${population}</p></li>
-      <li><p><span class="country-meta__title">Languages</span>:${language}</p></li>
-    </ul>
-  </li>`
+    </div>
+    </a>
+    `
 }
-
-
-
